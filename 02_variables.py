92a96359be01e395c69e615e4647da973d04734a
brockelsbya1148 <110875387+brockelsbya1148@users.noreply.github.com>
2022-08-11 11:53:38 +1200

    Create 02_variables.py

---
+++ 02_variables.py
@@ -0,0 +1,21 @@
+# code to learn about variables
+from time import sleep
+#variables can be a string
+greeting = "hello world"
+
+# they can also be numbers
+fav_num = 69
+
+# this variable has a problem
+my_name = "Ashden"
+
+print(greeting)
+print()
+sleep(1.25)
+
+print(fav_num)
+print()
+sleep(1.25)
+
+print(my_name)
+print()
